fc1c3010540b6ab857f73258f91c5b3ede78f2c4
Radithya Ihza <ihzabowo96@gmail.com>
2025-01-21 06:05:18 +0700

    fixed assets url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://ai-insider-t9fvshfur-anthesis-projects.vercel.app/'),
 
     /*
     |--------------------------------------------------------------------------
